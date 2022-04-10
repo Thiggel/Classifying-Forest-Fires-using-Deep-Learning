8958e9a607dbef91bb54341e9e1e2b5a3403e630
Filipe Laitenberger <f.laitenberger@gmail.com>
2022-04-10 17:08:58 +0200

    .

--- Lenet5.py
+++ Lenet5.py
@@ -1,6 +1,5 @@
 """
-Author: Mohamed Gamil
-        Thijs van der Laan
+Author: Mohamed Gamil, Thijs van der Laan
 """
 
 import torch.nn as nn
@@ -20,28 +19,36 @@ class LeNet5(TreeClassificationModel):
         super().__init__(learning_rate, filename)
 
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 6, kernel_size=(5, 5), stride=(1, 1), padding=0),
+            nn.Conv2d(3, 6, kernel_size=5, stride=1, padding=0),
             nn.BatchNorm2d(6),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride = 2)
         )
-        
+
         self.layer2 = nn.Sequential(
             nn.Conv2d(6, 16, kernel_size=(5, 5), stride=(1, 1), padding=0),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        
-        self.fc = nn.Linear(7744, 120)
-        self.relu = nn.ReLU()
 
         self.layer3 = nn.Sequential(
-            nn.Conv2d(120, 84, kernel_size=(5, 5), stride=(1, 1), padding=0),
+            nn.Conv2d(16, 120, kernel_size=(5, 5), stride=(1, 1), padding=0),
+            nn.BatchNorm2d(120),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
+        self.flatten = nn.Flatten()
+
+        # fully connected
+        self.fc = nn.Linear(9720, 84)
+        self.relu = nn.ReLU()
+
+        # output layer
         self.fc2 = nn.Linear(84, num_classes)
+
+        self.softmax = nn.Softmax()
 
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -51,10 +58,9 @@ class LeNet5(TreeClassificationModel):
         """
         out = self.layer1(x)
         out = self.layer2(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        out = self.relu(out)
         out = self.layer3(out)
-        out = self.fc2(out)
+        out = self.flatten(out)
+        out = self.relu(self.fc(out))
+        out = self.softmax(self.fc2(out))
 
         return out
